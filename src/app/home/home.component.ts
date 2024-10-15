173e604fc75f6256d6b51f18a923a7a594294cda
ramajonnada <rama.jonnada23@gmail.com>
2024-10-15 15:18:54 +0530

    add tool-bar for home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule,RouterModule],
+  imports: [MatButtonModule, RouterModule, MatToolbarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
